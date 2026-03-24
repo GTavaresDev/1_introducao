fb378f3c670046dc2bcca522db3c03668f2c96ea
Tavares <tavares.gabriel.dev@gmail.com>
2026-03-24 11:21:36 -0300

    Implementar página inicial com saudação e link para a página "Sobre"; adicionar página "Sobre" com informações da empresa e navegação de volta à página inicial.

---
+++ src/app/sobre/page.js
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+const SobrePage = () => {
+  return (
+    <div>
+      <h1>Sobre a empresa</h1>
+      <p>Esta é a página sobre a empresa.</p>
+      <Link href="/">Voltar para home</Link>
+    </div>
+  );
+};
+
+export default SobrePage;
